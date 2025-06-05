ccbcb693c0b6d4fa265b830894c1eafb0160355f
Adzka <zahratuadzka@gmail.com>
2025-06-05 16:45:17 +0700

    hihi

--- src/main/java/com/bni/bni/controller/AuthController.java
+++ src/main/java/com/bni/bni/controller/AuthController.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -13,6 +14,12 @@ import java.util.HashMap;
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+//
+//    @Value("${CONFIG_MAP_VALUE:default-config}")
+//    private String configMapValue;
+
+//    @Value("${SECRET_VALUE:default-secret}")
+//    private String secretValue;
 
     @Autowired
     private AuthService authService;
@@ -23,8 +30,9 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<Map<String, Object>> register(@RequestBody Map<String, String> body) {
         String username = body.get("username");
+        String emailAddress = body.get("emailAddress"); // tambahkan ini
         String password = body.get("password");
-        String message = authService.register(username, password);
+        String message = authService.register(username, emailAddress, password);
 
         Map<String, Object> response = new HashMap<>();
         response.put("status", 200);
@@ -43,11 +51,11 @@ public class AuthController {
         if (token != null) {
             response.put("status", 200);
             response.put("token", token);
-            response.put("message", "AAN ANDROID BKT");
+            response.put("message", "Login Berhasil");
             return ResponseEntity.ok(response);
         } else {
             response.put("status", 401);
-            response.put("message", "Invalid credentials");
+            response.put("message", "Token Tidak Valid");
             return ResponseEntity.status(401).body(response);
         }
     }
@@ -75,7 +83,7 @@ public class AuthController {
 
             response.put("status", 200);
             response.put("username", claims.getSubject());
-            response.put("role", claims.get("role"));
+            // Hapus response.put("role", ...)
             response.put("issuedAt", claims.getIssuedAt());
             response.put("expiration", claims.getExpiration());
 
@@ -87,4 +95,5 @@ public class AuthController {
             return ResponseEntity.status(401).body(response);
         }
     }
+
 }
